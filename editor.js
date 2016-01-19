5c9c622154dec2a5b197f72195ffdb58872780a8
claudius108 <claudius.teodorescu@gmail.com>
2016-01-19 14:39:06 +0100

    Deleted not needed button.
    References #1548.

--- editor.js
+++ editor.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     fluxProcessor.skipshutdown = true;
     
     $("#tabs-container").tabs({
-        activate: function(ev, ui) {
+        activate: function(event, ui) {
             $("div.subtabs", ui.newPanel).tabs("option", "active", 0);
             fluxProcessor.dispatchEventType("main-content", "loadSubform", {
                 "subformId": $("div.subtabs li:first-child", ui.newPanel).attr('aria-controls')
